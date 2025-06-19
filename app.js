2ad1fbf1853b49de6511b3c8bd3008b7f6b05440
sultaaaane <mohamadalaoui77@gmail.com>
2025-06-19 11:24:20 +0100

    making jwt refresh token

--- app.js
+++ app.js
@@ -9,29 +9,25 @@ import fastifyStatic from "@fastify/static";
 import prismaPlugin from "./plugins/prisma.js";
 import { authRoutes } from "./routes/auth.js";
 
-// Needed for __dirname in ESM
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Load environment variables
 dotenv.config();
 
-// Create Fastify instance
 const fastify = Fastify({ logger: true });
 
 fastify.register(fastifyStatic, {
   root: path.join(__dirname, "public"),
 });
-// 1. Register core plugins
+
 await fastify.register(formbody);
+
 await fastify.register(jwt, {
   secret: process.env.JWT_SECRET || "supersecret",
 });
 
-// 2. Register Prisma plugin
 await fastify.register(prismaPlugin);
 
-// 3. Add JWT auth decorator
 fastify.decorate("authenticate", async function (request, reply) {
   try {
     await request.jwtVerify();
@@ -40,13 +36,12 @@ fastify.decorate("authenticate", async function (request, reply) {
   }
 });
 
-// 4. Register routes
 await fastify.register(authRoutes);
 
 fastify.get("/", async (_, reply) => {
   reply.redirect("index.html");
 });
-// 5. Start server
+
 const start = async () => {
   try {
     await fastify.listen({ port: 3000, host: "0.0.0.0" });
